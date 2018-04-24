f982b9d56fc401580da0b3b2c87df4a6df2d7c01
tanakorn0314 <tanakorn0412@gmail.com>
2018-04-25 02:26:38 +0700

    58

--- TestThread.java
+++ TestThread.java
@@ -4,27 +4,112 @@ public class TestThread
 {
 	public static void main(String[] args)
 	{
-		Queue<Integer> q = new LinkedList<Integer>();
+		SyncQueue<Integer> sq = new SyncQueue<Integer>();
 
-		for(int i=0;i<5;i++)
+		Thread p = new Thread(new Producer(sq));
+		Thread c = new Thread(new Consumer(sq));
+
+		p.start();
+		c.start();
+	}
+
+	public static class SyncQueue<DataType>
+	{
+		public LinkedList<DataType> l;
+		private boolean available = false;
+
+		public SyncQueue()
 		{
-			Thread t = new Thread(new QueueProducer(q,i));
-			t.start();
-			try
+			l = new LinkedList<DataType>();
+		}
+
+		public synchronized void add(DataType x)
+		{
+			while(available==true)
 			{
-				t.join();
-				Thread t2 = new Thread(new QueueConsumer(q));
-				t2.start();
-				// t2.join();
+				try
+				{
+					wait();
+				}catch(InterruptedException ie){}
 			}
-			catch(InterruptedException ie)
+			l.addLast(x);
+				available=true;
+			System.out.println("Put!");
+			System.out.println("current list : " + this); 
+			notifyAll();
+		}
+
+		public synchronized DataType remove()
+		{
+			while(available==false)
 			{
-				System.out.println("Interrupted");
+				try
+				{
+					wait();
+				}catch(InterruptedException ie){}
+			}
+			DataType first = l.removeFirst();
+				available=false;
+			System.out.println("Get : " + first);
+			notifyAll();
+
+			return first;
+		}
+
+		public String toString()
+		{
+			String s = "";
+			for(DataType i:l)
+				s = s+i+" ";
+			return s;
+		}
+	}
+
+	public static class Consumer implements Runnable
+	{
+		private SyncQueue<Integer> sq;
+
+		public Consumer(SyncQueue<Integer> syncQueue)
+		{
+			sq = syncQueue;
+		}
+
+		public void run()
+		{
+			while(true)
+			{
+				sq.remove();
 			}
 			
 		}
-
-		System.out.println("Terminated");
 	}
-		
-}
+
+	public static class Producer implements Runnable
+	{
+		private SyncQueue<Integer> sq;
+		private boolean available;
+
+		public Producer(SyncQueue<Integer> syncQueue)
+		{
+			sq = syncQueue;
+			available = true;
+		}
+
+		public void run()
+		{
+			while(true)
+			{
+				Integer r = (int)(Math.random()*20+1);
+				sq.add(r);
+				sleep(2000);
+			}
+		}
+	}
+	
+	public static void sleep(long t)
+	{
+		try{
+				Thread.sleep(t);
+			}catch(InterruptedException e){}
+	}
+}	
